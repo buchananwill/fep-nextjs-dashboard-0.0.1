f59d14d81cee8a308f53d6ba100aec7911d947bc
buchananwill <139961977+buchananwill@users.noreply.github.com>
2023-11-10 12:42:14 +0000

    Fixed some Vercel build errors.

--- app/contact-time/all-subjects-scatterchart.tsx
+++ app/contact-time/all-subjects-scatterchart.tsx
@@ -35,53 +35,55 @@ export default function SubjectContactTime({
     );
     const sizeRange = [minSize, maxSize];
 
-    return (
-      <Card className="mt-8">
-        <Title>Subject Contact Time</Title>
-        <Text>
-          Summarising the total contact time required per subject for this
-          academic year.
-        </Text>
-        <ScatterChart
-          className="mt-4 h-80"
-          data={filteredSubjects}
-          maxYValue={
-            filteredSubjects.reduce(
-              (max, dataPoint) =>
-                dataPoint.perTeacher > max ? dataPoint.perTeacher : max,
-              filteredSubjects[0].perTeacher
-            ) * 1.1
-          }
-          category="subject"
-          x="periods"
-          y="perTeacher"
-          size="teachers"
-          // sizeRange={sizeRange}
-          showOpacity={true}
-          minYValue={0}
-          colors={[
-            'red',
-            'orange',
-            'amber',
-            'yellow',
-            'lime',
-            'green',
-            'emerald',
-            'teal',
-            'cyan',
-            'sky',
-            'blue',
-            'indigo',
-            'violet',
-            'purple',
-            'fuchsia',
-            'pink',
-            'rose'
-          ]}
-          valueFormatter={formatter}
-          showLegend={false}
-        />
-      </Card>
-    );
+    if (filteredSubjects.length > 0) {
+      return (
+        <Card className="mt-8">
+          <Title>Subject Contact Time</Title>
+          <Text>
+            Summarising the total contact time required per subject for this
+            academic year.
+          </Text>
+          <ScatterChart
+            className="mt-4 h-80"
+            data={filteredSubjects}
+            maxYValue={
+              filteredSubjects.reduce(
+                (max, dataPoint) =>
+                  dataPoint.perTeacher > max ? dataPoint.perTeacher : max,
+                filteredSubjects[0].perTeacher
+              ) * 1.1
+            }
+            category="subject"
+            x="periods"
+            y="perTeacher"
+            size="teachers"
+            // sizeRange={sizeRange}
+            showOpacity={true}
+            minYValue={0}
+            colors={[
+              'red',
+              'orange',
+              'amber',
+              'yellow',
+              'lime',
+              'green',
+              'emerald',
+              'teal',
+              'cyan',
+              'sky',
+              'blue',
+              'indigo',
+              'violet',
+              'purple',
+              'fuchsia',
+              'pink',
+              'rose'
+            ]}
+            valueFormatter={formatter}
+            showLegend={false}
+          />
+        </Card>
+      );
+    } else return <>error...</>;
   } else return <>error...</>;
 }
