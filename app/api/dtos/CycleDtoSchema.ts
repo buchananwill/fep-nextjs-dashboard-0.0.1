000f357afe90e34f682709d3b8200238e6e67090
buchananwill <139961977+buchananwill@users.noreply.github.com>
2024-04-12 18:22:26 +0100

    Updated dto generation.

--- app/api/dtos/CycleDtoSchema.ts
+++ app/api/dtos/CycleDtoSchema.ts
@@ -1,9 +1,8 @@
-import { CycleSubspanDtoSchema } from './CycleSubspanDtoSchema';
 import { zDayOfWeek } from '../zod-mods';
 import { z } from 'zod';
 export const CycleDtoSchema = z.object({
   id: z.number(),
   cycleLengthInDays: z.number(),
-  cycleDayZero: zDayOfWeek
+  cycleDayZero: zDayOfWeek,
 });
 export type CycleDto = z.infer<typeof CycleDtoSchema>;
--- app/api/dtos/ServiceCategoryDtoSchema.ts
+++ app/api/dtos/ServiceCategoryDtoSchema.ts
@@ -1,8 +1,11 @@
+import { CollectionSchema } from './CollectionSchema';
 import { z } from 'zod';
 export const ServiceCategoryDtoSchema = z.object({
   name: z.string(),
   id: z.number(),
   knowledgeDomainDescriptor: z.string(),
   knowledgeLevelDescriptor: z.string(),
+  knowledgeDomainIds: CollectionSchema,
+  knowledgeLevelIds: CollectionSchema,
 });
 export type ServiceCategoryDto = z.infer<typeof ServiceCategoryDtoSchema>;
